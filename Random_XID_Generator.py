f720dae111fd51f3090ede801f2e330865fec0c7
Pranav Goswami <pranavgeekme@gmail.com>
2015-06-26 13:58:33 +0530

    added file descriptor for generating input file

--- Random_XID_Generator.py
+++ Random_XID_Generator.py
@@ -1,9 +1,13 @@
-
+__author__ = 'Pranav'
 import string,random  
 def Random_4bytes_XIA():
   length=4
   letters=string.ascii_lowercase+string.digits
   return ''.join([random.choice(letters) for _ in range(length)])
 
-for i in range(1,320):
-	print Random_4bytes_XIA()
+for i in range(1,50000):
+
+    f = open('/Users/Pranav/Desktop/input_xia.txt','a')
+    value = Random_4bytes_XIA()
+    print >>f,value
+    f.close()
